0d1d61407f6e4793d27359f64d7abf6ee2ae2204
felixia2208 <105896280+felixia2208@users.noreply.github.com>
2022-05-20 12:18:40 +0700

    first upload

---
+++ database.js
@@ -0,0 +1,114 @@
+let makanan = [];
+
+let makanan_detail = {
+    foto: "https://images.app.goo.gl/uX7cn84NruQG5GH4A",
+    harga: 12000,
+    kode: "BRG4029",
+    nama: "Cappucino",
+    rating: "5.0",
+    stock: 20,
+
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/rG9zhJ8W6dQWLnwb7",
+    harga: 14500,
+    kode: "BRG4546",
+    nama: "Jus jeruk",
+    rating: "4.3",
+    stock: 10,
+
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/QKPEzxB4P6g517g97",
+    harga: 20000,
+    kode: "BRG6789",
+    nama: "Soto ayam",
+    rating: "4.8",
+    stock: 19,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/Y2F7nqzrbg9ByRLA8",
+    harga: 20000,
+    kode: "BRG6789",
+    nama: "Mie Ayam",
+    rating: "5.0",
+    stock: 25,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/yUQAy33r5YttpiB47",
+    harga: 6000,
+    kode: "BRG7889",
+    nama: "Croffle",
+    rating: "4.9",
+    stock: 36,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/JbGgzrgTWMNmHjgt7",
+    harga: 6500,
+    kode: "BRG5403",
+    nama: "Cookies",
+    rating: "5.0",
+    stock: 40,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/LWDvvZq7DhmyMBQ49",
+    harga: 5500,
+    kode: "BRG3488",
+    nama: "Souffle",
+    rating: "4.9",
+    stock: 35,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/rxMwdV9rpDDuuQyZ9",
+    harga: 10500,
+    kode: "BRG7532",
+    nama: "Matcha",
+    rating: "4.0",
+    stock: 26,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/ppboP1Ua4swG91jU9",
+    harga: 16000,
+    kode: "BRG9986",
+    nama: "Boba tea",
+    rating: "4.6",
+    stock: 15,
+};
+
+makanan.push(makanan_detail)
+
+makanan_detail = {
+    foto: "https://images.app.goo.gl/VwkSSzEk6es1fTfE7",
+    harga: 25000,
+    kode: "BRG3890",
+    nama: "Sauerkraut",
+    rating: "5.0",
+    stock: 20,
+};
+
+makanan.push(makanan_detail)
+console.log(makanan)
